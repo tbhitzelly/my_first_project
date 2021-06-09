9ed19de96d7ae67ac85f734ac8eda25b83f44e84
Aelita <aelitamirlanova72@gmail.com>
2021-06-09 21:41:27 +0600

    shell

--- course/models.py
+++ course/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Branch(models.Model):
     class Meta:
-        verbose_name='branch'
-        verbose_name_plural='branches'
+        verbose_name='филиал'
+        verbose_name_plural='филиалы'
 
     name = models.CharField(max_length=100, null=False)
     address = models.CharField(max_length=300, null=True)
@@ -15,8 +15,8 @@ class Branch(models.Model):
 
 class Group(models.Model):
     class Meta:
-        verbose_name='group'
-        verbose_name_plural='groups'
+        verbose_name='группа'
+        verbose_name_plural='группы'
 
     name = models.CharField(max_length=100, null=False)
     Branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
@@ -35,14 +35,16 @@ class Student(models.Model):
     )
 
     class Meta:
-        verbose_name='student'
-        verbose_name_plural='students'
+        verbose_name='студент'
+        verbose_name_plural='студенты'
 
     name = models.CharField(max_length=200)
     date_of_birth = models.DateField()
     address = models.CharField(max_length=300, null=True, blank=True)
     phone_number = models.CharField(max_length=20)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=6, default=MALE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.name
