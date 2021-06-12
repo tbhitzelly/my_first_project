0ec154ee85e9745b7dcef465f3800e5858515059
Aelita <aelitamirlanova72@gmail.com>
2021-06-12 21:32:24 +0600

    base.html

--- course/urls.py
+++ course/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import branches_list, group_list, my_main_page, student_list
+from course.views import branch_detail, group_detail, group_list, my_main_page, branches_list, student_detail, student_list
 
 urlpatterns = [
     path('', my_main_page, name='my_main_page'),
     path('branches/', branches_list, name='branches_list'),
-    path('student/', student_list, name='student_list'),
-    path('groups/', group_list, name='group_list')
+    path('branches/<int:branch_id>/', branch_detail, name='branch_detail'),
+    path('groups/', group_list, name='group_list'),
+    path('groups/<int:group_id>/', group_detail, name='group_detail'),
+    path('students/', student_list, name='student_list'),
+    path('students/<int:student_id>/', student_detail, name='student_detail')
 ]
--- course/views.py
+++ course/views.py
@@ -15,12 +15,33 @@ def branches_list(request):
     my_context = {'branches': branches}
     return render(request, 'course/branches_list.html', my_context)
 
-def student_list(request):
-    student = Student.objects.all()
-    my_context = {'student': student}
-    return render(request, 'course/student_list.html', my_context)
+
+def branch_detail(request, branch_id):
+    branch = Branch.objects.get(id=branch_id)
+    groups = Group.objects.filter(Branch=branch)
+    context = {'branch': branch, 'groups': groups}
+    return render(request, 'course/branch_detail.html', context=context)
 
 def group_list(request):
     groups = Group.objects.all()
     my_context = {'groups': groups}
-    return render(request, 'course/group_list.html', my_context)
+    return render(request, 'course/group_list.html', context=my_context)
+
+
+def group_detail(request, group_id):
+    group = Group.objects.get(id=group_id)
+    students = Student.objects.filter(group=group)
+    context = {'group': group, 'students': students}
+    return render(request, 'course/group_detail.html', context=context)
+
+
+def student_list(request):
+    students = Student.objects.all()
+    my_context = {'students': students}
+    return render(request, 'course/student_list.html', context=my_context)
+
+
+def student_detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    my_context = {'student': student}
+    return render(request, 'course/student_detail.html', context=my_context)
